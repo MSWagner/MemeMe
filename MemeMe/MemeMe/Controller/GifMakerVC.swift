35385fb91a2a118032ff2979cdb8891dfe020d25
Michael Wagner <24782257+MSWagner@users.noreply.github.com>
2017-11-19 23:15:47 +0100

    add IBOutlets and placholder Image

--- MemeMe/MemeMe/Controller/GifMakerVC.swift
+++ MemeMe/MemeMe/Controller/GifMakerVC.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class GifMakerVC: UIViewController {
-    @IBOutlet weak var placeholderView: UIView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
