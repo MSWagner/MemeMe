1e74994591485bb529d730587b36f2a1fcb69c05
Matthias Wagner <24782257+MSWagner@users.noreply.github.com>
2017-12-03 19:12:30 +0100

    add MemeCollectionCell

---
+++ MemeMe/MemeMe/View/MemeCollectionCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionCell.swift
+//  MemeMe
+//
+//  Created by Matthias Wagner on 03.12.17.
+//  Copyright © 2017 Michael Wagner. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionCell: UICollectionViewCell {
+    @IBOutlet weak var memeImage: UIImageView!
+}
