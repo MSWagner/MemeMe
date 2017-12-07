e89d3f07f41864fbb5addee09258ca07c8bdf200
Matthias Wagner <24782257+MSWagner@users.noreply.github.com>
2017-12-07 21:37:56 +0100

    del useless code

--- MemeMe/MemeMe/View/ReusableCells/MemeTableCell.swift
+++ MemeMe/MemeMe/View/ReusableCells/MemeTableCell.swift
@@ -12,16 +12,4 @@ class MemeTableCell: UITableViewCell {
     @IBOutlet weak var memeImage: UIImageView!
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var bottomLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
